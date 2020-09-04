f3e824e88de8e01addbb833c37c26e7a474962fa
Julio Altamiranda <julioaltamiranda77@gmail.com>
2020-09-04 14:32:14 -0300

    views layout

---
+++ resources/views/admin/layouts/includes/scripts.blade.php
@@ -0,0 +1,22 @@
+  <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="/assets/plugins/jquery/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="/assets/plugins/popper/popper.min.js"></script>
+    <script src="/assets/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="/assets/js/perfect-scrollbar.jquery.min.js"></script>
+    <!--Wave Effects -->
+    <script src="/assets/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="/assets/js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="/assets/js/custom.min.js"></script>
+    @stack('scripts')
---
+++ resources/views/admin/layouts/includes/sidebar.blade.php
@@ -0,0 +1,52 @@
+<nav class="sidebar-nav">
+    <ul id="sidebarnav">
+        <li>
+            <a class="waves-effect waves-dark" href="http://eliteadmin.themedesigner.in/demos/bt4/documentation/documentation.html" aria-expanded="false">
+                <i class="ti-home"></i>
+                <span class="hide-menu">Inicio</span>
+            </a>
+        </li>
+        <li>
+            <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                <i class="ti-layout-media-right-alt"></i>
+                <span class="hide-menu">Blog                    
+                </span>
+            </a>
+            <ul aria-expanded="false" class="collapse">
+                <li>
+                    <a href="index.html">Todos los posts </a>
+                </li>
+                <li>
+                    <a href="index2.html">Crear un post</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                <i class="ti-user"></i>
+                <span class="hide-menu">Usuarios
+                </span>
+            </a>
+            <ul aria-expanded="false" class="collapse">
+                <li>
+                    <a href="index.html">Todos los usuarios </a>
+                </li>
+                <li>
+                    <a href="index2.html">Crear un usuario</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a class="waves-effect waves-dark" aria-expanded="false">
+                <i class="ti-hand-open"></i>
+                <span class="hide-menu">Roles</span>
+            </a>
+        </li>
+        <li>
+            <a class="waves-effect waves-dark" aria-expanded="false">
+                <i class="ti-key"></i>
+                <span class="hide-menu">Permisos</span>
+            </a>
+        </li>
+    </ul>
+</nav>
